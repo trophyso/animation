794b3d41866abad3c0c556349de820b96d08ee9d
Charlie Brinicombe <55358291+cbrinicombe13@users.noreply.github.com>
2025-07-24 10:40:31 +0100

    Add social champ

--- src/GenericThumbnailImage/GenericThumbnailImage.tsx
+++ src/GenericThumbnailImage/GenericThumbnailImage.tsx
@@ -2,13 +2,14 @@ import { getInputProps } from "remotion";
 import { ThumbnailImage } from "../components/ThumbnailImage";
 
 export const GenericThumbnailImage: React.FC = () => {
-    let { title } = getInputProps();
+    const { title: titleInput, imageUrl } = getInputProps();
+    let title = titleInput;
 
     if (!title) {
         title = "Enter title here";
     }
 
     return (
-        <ThumbnailImage title={title as string} imageUrl="assets/duolingo_energy.png" />
+        <ThumbnailImage title={title as string} imageUrl={imageUrl as string} />
     );
 };
--- src/SalesDocImage/SalesDocImage.tsx
+++ src/SalesDocImage/SalesDocImage.tsx
@@ -44,7 +44,7 @@ export const SalesDocImage: React.FC = () => {
                         }}
                     />
                     <Img
-                        src={staticFile('brand/sales_logo_bamboo.png')}
+                        src={staticFile('brand/sales_logo_socialchamp.png')}
                         style={{ width: 250 }}
                     />
                 </AbsoluteFill>
@@ -66,7 +66,7 @@ export const SalesDocImage: React.FC = () => {
                             letterSpacing: "0.25rem"
                         }}
                     >
-                        PROJECT PLAN
+                        INTEGRATION PROPOSAL
                     </p>
                 </AbsoluteFill>
             </AbsoluteFill>
